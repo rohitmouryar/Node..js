a6023ef6299be335f1874f587b2ef813425a994a
rohitmouryar <125853279+rohitmouryar@users.noreply.github.com>
2026-04-28 12:08:29 +0530

    Why is a 404 page created and how does it work.

---
+++ Express.js/24_6_ Make 404 Page/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+    const absPath = path.resolve('file');
+
+
+app.get("/", (req, res) => {
+    res.sendFile(absPath+'/home.html'); // ✅ fix
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(absPath+'/login.html'); // ✅ fix
+});
+
+app.get("/about", (req, res) => {
+   res.sendFile(absPath+'/about.html'); // ✅ fix
+});
+
+app.use((req,res)=>{
+    res.status(404).sendFile(absPath+'/404.html')
+})
+app.listen(3100, () => {
+    console.log("Server running on http://localhost:3100");
+});
